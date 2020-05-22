b51b1776d8a202b8c9126f8d18eafd2ee0885f12
Daysure <das.yaegar@gmail.com>
2020-05-22 14:04:30 +1000

    created mailtrap.io account

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, request
-# from flask_sqlalchemy import SQLAlchemy
-# from send_mail import send_mail
+from flask_sqlalchemy import SQLAlchemy
+from send_mail import send_mail
 
 app = Flask(__name__)
 
-# ENV = 'dev'
+ENV = 'dev'
 
-# if ENV == 'dev':
-#     app.debug = True
-#     app.config['SQLALCHEMY_DATABASE_URI'] = ''
-# else:
-#     app.debug = False
-#     app.config['SQLALCHEMY_DATABASE_URI'] = ''
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ausgrid853890@localhost/simple-feedback-app'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
-# class Feedback(db.Model):
-#     __tablename__ = 'feedback'
-#     id = db.Column(db.Integer, primary_key=True)
-#     customer = db.Column(db.String(200), unique=True)
-#     dealer = db.Column(db.String(200))
-#     rating = db.Column(db.Integer)
-#     comments = db.Column(db.Text())
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
 
-#     def __init__(self, customer, dealer, rating, comments):
-#         self.customer = customer
-#         self.dealer = dealer
-#         self.rating = rating
-#         self.comments = comments
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 
 @app.route('/')
@@ -58,5 +58,4 @@ def submit():
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
